7b7601d12a687897b26b2fbff2c05b296542b38c
Aleksandr Roslyakov <torpid@gmail.com>
2018-02-23 19:58:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,4 +2,5 @@ var playlist = {U2: "Sunday Bloody Sunday", Coldplay: "Yellow"}
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist.artistName = "songTitle"
+  return playlist
 }
